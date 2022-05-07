59bbd021646308e851cc57ac61eec1f319c882cf
Onii Chan <66708802+me-oniichan@users.noreply.github.com>
2022-05-07 12:39:37 +0530

    Add files via upload

---
+++ selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Size of array : ";
+    cin>>num;
+    cout<<endl;
+
+    int arr[num]; int elem;
+    cout<<"Enter elements for array"<<endl;
+    for (int i = 0; i<num; i++){
+        cin>>elem;
+        arr[i] = elem;
+    }
+
+// selection sort
+    for (int i = 0; i < num; i++){
+        for (int j = i; j < num; j++){
+            if (arr[i] > arr[j]){
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+   for (int i = 0; i < num; i++){
+       cout<<arr[i]<<" ";
+   }
+}
