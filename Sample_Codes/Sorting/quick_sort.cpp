48fe8da89e3bd88f2ab9af306f634de157d1c757
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-12-27 19:11:26 +0530

    Quick Sort

--- Sample_Codes/Sorting/quick_sort.cpp
+++ Sample_Codes/Sorting/quick_sort.cpp
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
 int partition(int arr[], int low, int high)
 {
     int pivot = arr[high];
     int i = (low - 1);
-    for (int j = low; j <= high; j++)
+    for (int j = low; j <= high - 1; j++)
     {
-        if (arr[high] < pivot)
+        if (arr[j] < pivot)
         {
             i++;
             swap(arr[i], arr[j]);
@@ -17,30 +24,27 @@ int partition(int arr[], int low, int high)
     return (i + 1);
 }
 
-void quickSort(int arr[], int low, int high)
+void quickSort(int arr[], int low, int high, int n)
 {
-    if(low<high){
+    if (low < high)
+    {
+        printArray(arr, n);
         int pi = partition(arr, low, high);
-        quickSort(arr, low, pi-1);
-        quickSort(arr, pi+1, high);
+        quickSort(arr, low, pi - 1, n);
+        quickSort(arr, pi + 1, high, n);
     }
-}
-
-void printArray(int arr[], int n){
-    for(int i=0;i<n;i++)
-    cout<<arr[i]<<" ";
 }
 
 int main()
 {
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
-    int arr[n];
-    cout << "Enter array elements: ";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    quickSort(arr, 0, n - 1);
+    int n=10;
+    // cout << "Enter size of array: ";
+    // cin >> n;
+    int arr[n] = {10,9,8,7,6,5,4,3,2,1};
+    // cout << "Enter array elements: ";
+    // for (int i = 0; i < n; i++)
+    //     cin >> arr[i];
+    quickSort(arr, 0, n - 1, n);
     printArray(arr, n);
     return 0;
 }
