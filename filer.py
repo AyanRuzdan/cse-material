ca8ef5d699904ca6ec2dfdfb0e96c5a801764af7
AyanRuzdan <ayanruzdan@gmail.com>
2025-03-22 04:30:38 +0530

    vault backup: 2025-03-22 04:30:38
    Affected files:
    filer.py

--- filer.py
+++ filer.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 def get_files_in_directory(output_csv):
-    directory = os.getcwd()  # Scan the current working directory (GitHub Actions workspace)
+    directory = os.getcwd()
     file_data = []
 
     for root, _, files in os.walk(directory):
@@ -17,21 +17,14 @@ def get_files_in_directory(output_csv):
                 z = "/".join(z)
                 if file_size == 0:
                     file_data.append([z[:-3], file_size])
-
-    # Create a new DataFrame with the file data
     df = pd.DataFrame(file_data, columns=["File Path", "Size (Bytes)"])
-
-    # Append current timestamp
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     timestamp_df = pd.DataFrame([[timestamp]], columns=["Timestamp"])
     df = pd.concat([df, timestamp_df], ignore_index=True)
-
-    # Save the updated CSV (overwrite mode)
     df.to_csv(output_csv, index=False, encoding='utf-8')
     print(
         f"CSV file saved at: {output_csv} with {len(file_data)} new entries. Timestamp updated.")
 
 
-# Example usage
 output_csv_file = "file_list.csv"
 get_files_in_directory(output_csv_file)
